3a73c61ef20fff85bd40d098f11b8b4c401b11db
李辰 <2095055676@qq.com>
2024-11-13 20:35:22 +0800

    feat: 用户登录完善

--- src/controllers/UserProfile/userProfile.ts
+++ src/controllers/UserProfile/userProfile.ts
@@ -2,40 +2,25 @@ import { NextFunction, Request, Response } from 'express';
 import * as userProfileServices from '../../services/UserProfile/userProfile.js';
 import logger from '../../utils/logger.js';
 import { validate } from "uuid";
-export const upload = async (req: Request, res: Response, next: NextFunction): Promise<any> => {
+import { handleReturn } from '../../utils/handleReturn.js';
+export const upload = async (req: Request, res: Response): Promise<any> => {
   try {
     const detail = req.body;
     // 校验一次id
     const { id } = detail;
     if (!validate(id)) {
-      return {
-        code: 400,
-        data: null,
-        message: '用户id不合法',
-      }
+      return handleReturn(400, null, '用户id不合法', res);
     }
     const response = await userProfileServices.upload(detail);
     if (response) {
       logger.info(`request body: ${detail} 用户信息上传成功`)
-      return {
-        code: 201,
-        data: response,
-        message: '用户信息上传成功'
-      }
+      return handleReturn(201, response, '用户信息上传成功', res);
     } else {
       logger.error(`request body: ${detail} 用户信息上传失败`)
-      return {
-        code: 400,
-        data: null,
-        message: '用户信息上传失败'
-      }
+      return handleReturn(400, null, '用户信息上传失败', res);
     }
   } catch (error) {
     logger.error(error);
-    return {
-      code: 500,
-      data: null,
-      message: '服务器错误'
-    }
+    return handleReturn(500, null, '服务器错误', res);
   }
 }
